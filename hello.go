9652ac5f7402c44bc7d358225be923d0a350b3b3
herui <herui@mia.com>
2017-11-21 14:27:33 +0800

    add

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	_, b := 34, 35
+	println(b)
+	fmt.Println("Hello World !")
+	fmt.Printf("Hello, world or 你好，世界 or καλημ ́ρα κóσμ or こんにちは世界\n")
+}
+
